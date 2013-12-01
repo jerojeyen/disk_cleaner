48dd0d272bb8d1dd898d4817c7a857a0d068f0ed
Jerozan <jerozan.jeyen@gmail.com>
2013-12-01 18:01:52 +0100

    refactor actions

--- DiskCleaner/actions.h
+++ DiskCleaner/actions.h
@@ -1,4 +1,9 @@
-void set_log_path(char *options, char (*params)[100], char (*messages)[100]);
-void set_path(char *options, char (*params)[100], char (*messages)[100]);
-BOOL InstallMyService();
-BOOL DeleteMyService();
+#ifndef ACTIONS_H_
+# define ACTIONS_H_
+
+	void setLogPath(char *opts, char (*params)[100], char (*messages)[100]);
+	void setPath(char *opts, char (*params)[100], char (*messages)[100]);
+	BOOL InstallMyService();
+	BOOL DeleteMyService();
+
+#endif /* !ACTIONS_H_ */
