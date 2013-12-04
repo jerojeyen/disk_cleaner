24cf5b5372ba40c69c811d04507b815b1cb1fd41
GuilhouT <guilhout@gmail.com>
2013-12-04 12:59:52 +0100

    Refactor service.c and change macro values

--- DiskCleaner/service.c
+++ DiskCleaner/service.c
@@ -1,24 +1,28 @@
 #include <Windows.h>
-#include "process.h"
 #include "macros.h"
+#include "process.h"
 
 void WINAPI ServiceCtrlHandler(DWORD Opcode)
 {
-	switch (Opcode)	{
+	switch (Opcode)
+	{
+		case SERVICE_CONTROL_INTERROGATE:
+			break;
+		case SERVICE_CONTROL_STOP:
+			// Do nothing if the service is not running
+			if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+			{
+				break;
+			}
+			g_ServiceStatus.dwWin32ExitCode = 0;
+			g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+			SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+			break;
 		case SERVICE_CONTROL_PAUSE:
 			g_ServiceStatus.dwCurrentState = SERVICE_PAUSED;
 			break;
 		case SERVICE_CONTROL_CONTINUE:
 			g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
-			break;
-		case SERVICE_CONTROL_STOP:
-			g_ServiceStatus.dwWin32ExitCode = 0;
-			g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
-			g_ServiceStatus.dwCheckPoint = 0;
-			g_ServiceStatus.dwWaitHint = 0;
-			SetServiceStatus (g_ServiceStatusHandle, &g_ServiceStatus);
-			break;
-		case SERVICE_CONTROL_INTERROGATE:
 			break;
 		default:
 			break;
@@ -27,20 +31,22 @@ void WINAPI ServiceCtrlHandler(DWORD Opcode)
 
 void WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
 {
-	g_ServiceStatus.dwServiceType = SERVICE_WIN32;
 	g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+	g_ServiceStatus.dwCheckPoint = 1;
 	g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32;
 	g_ServiceStatus.dwWin32ExitCode = 0;
 	g_ServiceStatus.dwServiceSpecificExitCode = 0;
-	g_ServiceStatus.dwCheckPoint = 0;
-	g_ServiceStatus.dwWaitHint = 0;
-	g_ServiceStatusHandle = RegisterServiceCtrlHandler(MY_SERVICE_NAME, ServiceCtrlHandler);
-	if (g_ServiceStatusHandle == (SERVICE_STATUS_HANDLE)0)
+
+	g_StatusHandle = RegisterServiceCtrlHandler(MY_SERVICE_NAME, ServiceCtrlHandler);
+	if (g_StatusHandle == NULL)
+	{
 		return;
+	}
+
 	g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
 	g_ServiceStatus.dwCheckPoint = 0;
-	g_ServiceStatus.dwWaitHint = 0;
-	SetServiceStatus(g_ServiceStatusHandle, &g_ServiceStatus);
-	/**/
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
 	main_process();
 }
