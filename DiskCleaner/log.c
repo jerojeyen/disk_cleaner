7f47b042ffb289acb2102ec42beb0933b558258e
Jerozan <jerozan.jeyen@gmail.com>
2013-12-05 23:18:42 +0100

    refactor logs

--- DiskCleaner/log.c
+++ DiskCleaner/log.c
@@ -1,55 +1,69 @@
 #include <Windows.h>
 #include <stdio.h>
+#include <strsafe.h>
 #include "regedit.h"
 #include "errors.h"
-#include <strsafe.h>
 
-INT log_info(LPCTSTR to_log)
+INT addToLog(LPCTSTR message)
 {
-	HANDLE hFile;
-	DWORD written;
+	HANDLE h_log_file;
+	DWORD nb_byte_written;
+	char log[750];
+	char current_str[25];
 	BYTE log_path[255];
 	SYSTEMTIME current;
-	char log_row[750];
 
 	if (retrieve_var("log", log_path) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	hFile = CreateFile((LPCTSTR)log_path, FILE_APPEND_DATA, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hFile == INVALID_HANDLE_VALUE)
+
+	h_log_file = CreateFile((LPCTSTR)log_path, FILE_APPEND_DATA, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (h_log_file == INVALID_HANDLE_VALUE)
 		return (EXIT_FAILURE);
+
 	GetLocalTime(&current);
-	sprintf(log_row, "[%d-%.2d-%.2d %.2d:%.2d:%.2d] - %s\r\n", current.wYear, current.wMonth, current.wDay,  current.wHour, current.wMinute, current.wSecond, to_log);
-	if (!WriteFile(hFile, (LPCTSTR)log_row, strlen((LPCTSTR)log_row), &written, NULL)) {
-		CloseHandle(hFile);
+	
+	timeToStr(current_str, current);
+	
+	sprintf(log, "%s - %s\r\n", current_str, message);
+	if (!WriteFile(h_log_file, (LPCTSTR)log, strlen((LPCTSTR)log), &nb_byte_written, NULL)) {
+		CloseHandle(h_log_file);
 		return (EXIT_FAILURE);
 	}
-	CloseHandle(hFile);
+
+	CloseHandle(h_log_file);
 	return (EXIT_SUCCESS);
 }
 
-int log_info_supress(LPCTSTR to_log)
+int concatBeforeAddToLog(LPCTSTR first_message, LPCTSTR second_message)
 {
-	char full_log[750];
+	char log[750];
 
-	sprintf(full_log, "%s has been deleted", to_log);
-	log_info(full_log);
+	sprintf(log, "%s%s", first_message, second_message);
+	addToLog(log);
 	return (EXIT_SUCCESS);
 }
 
-int log_info_err_supp(LPCTSTR to_log, DWORD error_num)
+int addToLogHasBeenDeleted(LPCTSTR message)
 {
-	char full_log[750];
+	char log[750];
 
-	sprintf(full_log, "%s has not been deleted : %s", to_log, get_error_msg(error_num));
-	log_info(full_log);
+	sprintf(log, "%s has been deleted", message);
+	addToLog(log);
 	return (EXIT_SUCCESS);
 }
 
-int log_info_concat(LPCTSTR to_loga, LPCTSTR to_logb)
+int addToLogNotBeenDeleted(LPCTSTR message, DWORD error)
 {
-	char full_log[750];
+	char log[750];
 
-	sprintf(full_log, "%s%s", to_loga, to_logb);
-	log_info(full_log);
+	sprintf(log, "%s has not been deleted : %s", message, get_error_msg(error));
+	addToLog(log);
+	return (EXIT_SUCCESS);
+}
+
+int  timeToStr(char* str_time, SYSTEMTIME time)
+{
+	sprintf(str_time, "[%d-%.2d-%.2d %.2d:%.2d:%.2d]", time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute, time.wSecond);
 	return (EXIT_SUCCESS);
 }
--- DiskCleaner/path.c
+++ DiskCleaner/path.c
@@ -9,15 +9,15 @@ LPTSTR get_windows_temp_path()
 	LPTSTR path;
 
 	if (retrieve_machine_var("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", "TEMP", temp) == EXIT_FAILURE) {
-		log_info("No Windows Temp folder");
+		addToLog("No Windows Temp folder");
 		return ("");
 	}
 	if (retrieve_machine_var("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "SystemRoot", system_root) == EXIT_FAILURE)	{
-		log_info("No Windows Temp folder");
+		addToLog("No Windows Temp folder");
 		return ("");
 	}
 	path = strSub((LPTSTR)temp, "%SystemRoot%", (LPTSTR)system_root);
-	log_info_concat("Windows Temp folder found : ", path);
+	concatBeforeAddToLog("Windows Temp folder found : ", path);
 	return (path);
 }
 
@@ -27,10 +27,10 @@ LPTSTR get_DiskCleaner_path()
 	LPTSTR path;
 
 	if (retrieve_var("path", reg_path) == EXIT_FAILURE) {
-		log_info("Missing path to clean !");
+		addToLog("Missing path to clean !");
 		return ("");
 	}
 	path = strdup((LPTSTR)reg_path);
-	log_info_concat("Path to clean found : ", path);
+	concatBeforeAddToLog("Path to clean found : ", path);
 	return (path);
 }
